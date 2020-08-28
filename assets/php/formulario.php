bc7e40f0bb080745bce7590ca4c065b991eae1e4
ReyesVallejos <ser.reyesv@gmail.com>
2020-08-28 15:57:39 -0400

    formulario php

--- assets/php/formulario.php
+++ assets/php/formulario.php
@@ -12,6 +12,7 @@ $estructura = "<style>
 *{margin:0;padding:0;box-sizing:border-box;}
   .contenedor-email{border:1px solid #8a8a8a;padding: 10px;width: 60%; margin: auto;}
   .destacado {background-color: #EBC31E;}
+  .cuerpo-mensaje {margin:auto;}
 </style>";
 
 $estructura .= "<div class='contenedor-email'>
@@ -21,8 +22,8 @@ $estructura .= "<div class='contenedor-email'>
   <p>Hemos recibido un nuevo mensaje desde nuestra web!. Presta atención a la solicitud para poder satisfacer las necesidades de tus clientes.</p>
   <p>La solicitud fue enviada por <span class='destacado'> " . $nombre . "</span></p>
   <p>Su email es <span class='destacado'> " . $email . "</span> y su número teléfonico es <span class='destacado'> " . $telefono . "</span>.</p>
-  <p>y el mensaje enviado dice " . $mensaje . ".</p>
-  <p>Ponte en contacto con " . $nombre . " para resolver sus necesidades! <span style='font-size:30px;'>&#128512;</span></p>
+  <p>y el mensaje enviado dice: <br> " . $mensaje . ".</p>
+  <p>Ponte en contacto con " . $nombre . " para resolver sus necesidades! <span style='font-size:20px;'>&#128512;</span></p>
 </div>  
 </div>";
 
@@ -30,7 +31,6 @@ $estructura .= "<div class='contenedor-email'>
 $encabezado = 'Content-type:text/html;charset=UTF-8';
 mail($destinatario,$asunto,$estructura,$encabezado);
 
-header('location:/dist/index.html');
+header('location:/');
 
 ?>
-
